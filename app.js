4e57f1c2e6e5a50320f8404cb01c73b67b974bd9
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-08-04 04:33:08 -0500

    changes

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-document.getElementById('shareButton').addEventListener('click', function() {
-  const fileInput = document.getElementById('fileInput');
+document.getElementById("shareButton").addEventListener("click", function () {
+  const fileInput = document.getElementById("fileInput");
   const file = fileInput.files[0];
-  
+
   if (!file) {
-    alert('Please select a file to share.');
+    alert("Please select a file to share.");
     return;
   }
 
-  const downloadLink = document.getElementById('downloadLink');
   const fileURL = URL.createObjectURL(file);
-  downloadLink.href = fileURL;
-  downloadLink.download = file.name;
-  downloadLink.style.display = 'inline-block';
-  alert('File shared successfully!');
+  const fileLinkInput = document.getElementById("fileLink");
+  fileLinkInput.value = fileURL;
+  document.querySelector(".link").style.display = "block";
+});
+
+document.getElementById("copyButton").addEventListener("click", function () {
+  const fileLinkInput = document.getElementById("fileLink");
+  fileLinkInput.select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard!");
 });
